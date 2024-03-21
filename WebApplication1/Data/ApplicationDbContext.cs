772cc0f8dae998fc8b1892e5d28846c9d5725ce6
avijtt <infoavi9090@gmail.com>
2024-03-21 10:16:19 +0545

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+        //Category is model and categories is tablename
+        public DbSet<Category> categories { get; set; }
+
+        //default model buldet to show data 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DispalyOrder = 1 },
+                 new Category { Id = 2, Name = "History", DispalyOrder = 2 },
+                  new Category { Id = 3, Name = "Scifi", DispalyOrder = 3 }
+
+                );
+        }
+    }
+}
